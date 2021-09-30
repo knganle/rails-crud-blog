aeab03ece17833d5543e043b557458bccab0b753
Ngan Le <nganle@Ngans-MacBook-Pro.local>
2021-09-30 13:04:37 +0700

    fix minor

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,15 +1,20 @@
 class ArticlePolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
+  def show?
+    # (@user.has_role? :admin) || (@user.id == @record.user.id)
+    true
   end
 
   def update?
     @user.id == @record.user.id
   end
 
-  def destroy?
-    (@user.has_role? :admin) || (@user.id == @record.user.id)
+  # def destroy?
+  #   (@user.has_role? :admin) || (@user.id == @record.user.id)
+  # end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
   end
 end
